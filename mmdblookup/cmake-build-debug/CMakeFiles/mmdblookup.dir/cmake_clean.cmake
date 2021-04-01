b09bd72235b63c79a0c464de343aee3a1f3eca66
Ildar Zaynullin <zaynullinildar250995@gmail.com>
2021-04-01 23:58:55 +0300

    Add files via upload
    project uploading

---
+++ mmdblookup/cmake-build-debug/CMakeFiles/mmdblookup.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mmdblookup.dir/main.c.o"
+  "mmdblookup"
+  "mmdblookup.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mmdblookup.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
